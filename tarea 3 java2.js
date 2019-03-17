2c000411ffc65f2d06b8b12a847c3a8bba5f69fa
dbsvolante7@gmail.com <dbsvolante7@gmail.com>
2019-03-16 21:58:13 -0600

    tarea

---
+++ tarea 3 java2.js
@@ -0,0 +1,26 @@
+// declarar variables
+var wins1 = document.getElementById("wins");
+var losses1 = document.getElementById("losses");
+var left1 = document.getElementById("left");
+var gs1 = document.getElementById("gs");
+
+//acción
+
+document.onkeyup = function (event) { //
+
+    //Ligar tecla a html
+
+    //Escoger una letra 
+
+    var letras = ["a", "b", "c", "d", "e"]
+    var aleatorio = Math.random();
+    console.log "aleatorio";
+
+    //Compararla que escogio el usuario vs la computadora
+
+    // comparar si son iguales
+
+    // comparar si no son iguales
+    
+    
+}; 
